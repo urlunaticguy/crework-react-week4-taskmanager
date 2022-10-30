1db6986ee47c29f4cfe24a2c6024820ac41bf71d
Souvik Das <souvikvillain@icloud.com>
2022-10-30 16:23:11 +0530

    fixed margins and sizing

--- src/Components/Done.jsx
+++ src/Components/Done.jsx
@@ -23,7 +23,7 @@ function Done(props) {
   }
   
   return (
-    <div className=' text-yellow-400 backdrop-blur-md bg-[#0C1E2D]/10 w-[30vw] h-[40rem] border-4 border-white rounded-lg py-4 px-8'>
+    <div className=' text-yellow-400 backdrop-blur-md bg-[#0C1E2D]/10 w-[30vw] h-[65vh] border-4 border-white rounded-lg py-4 px-8'>
       {tD.length === 0 && 
         <h1 className=' text-[1.8rem]'>You have no tasks completed.</h1>
       }
@@ -36,10 +36,10 @@ function Done(props) {
             <svg className=' w-[1.4rem] fill-gray-200' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
               <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/>
             </svg>
-            <svg onClick={() => {delTask(taskObject, index)}} className=' cursor-pointer absolute w-[1.2rem] fill-red-600 hover:fill-red-300 ml-7 mt-[0.5%]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <svg onClick={() => {delTask(taskObject, index)}} className=' cursor-pointer absolute w-[1.2rem] fill-red-600 hover:fill-red-300 ml-[1.7rem] mt-[0.5%]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                 <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
             </svg>
-            <li className=' ml-[5%] cursor-pointer hover:underline text-[1.2rem] text-gray-200'>{taskObject}</li>
+            <li className=' ml-[1.4rem] cursor-pointer hover:underline text-[1.2rem] text-gray-200'>{taskObject}</li>
           </div>
         )}
       </ol>
--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -31,7 +31,7 @@ function ToDo(props) {
   }
   
   return (
-    <div className=' backdrop-blur-md w-[30vw] h-[40rem] bg-[#0C1E2D]/10 text-yellow-400 border-4 border-white rounded-lg py-4 px-8'>
+    <div className=' backdrop-blur-md w-[30vw] h-[65vh] bg-[#0C1E2D]/10 text-yellow-400 border-4 border-white rounded-lg py-4 px-8'>
       {tasksRemaining.length === 0 && 
         <h1 className=" text-[1.8rem]">You have no tasks remaining.</h1>
       }
@@ -49,7 +49,6 @@ function ToDo(props) {
               <svg className=' z-0 absolute w-[1.3rem] mt-[-2%] fill-white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                 <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/>
               </svg>
-              
             </div>
             <svg onClick={() => {deleteTask(taskObject, index)}} className=' cursor-pointer absolute w-[1.2rem] fill-red-600 hover:fill-red-300 ml-[1.8rem]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                 <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
